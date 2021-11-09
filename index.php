4d27fa329f3dbbd31ebbece52e2c9fce5ea92ac1
JeevanS1 <93318486+JeevanS1@users.noreply.github.com>
2021-11-09 18:29:48 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
 		
 		<div class="grid-container">
 			<div class="cards">
-				<h1 class="game_Title" align="center">Layout of the game</h1>
+				<h1 align="center">Layout of the game</h1>
 				<p>Age: 16+</p>
 				<p>Maximum Players: 4</p>
 				<button class="collapsible">More information</button>
@@ -57,7 +57,7 @@
 		
 		<div class="grid-container">
 			<div class="cards">
-				<h1 class="game_Title" align="center">Odd Number thrown</h1>
+				<h1 align="center">Odd Number thrown</h1>
 				<button class="collapsible">More information</button>
 	            <div class="collapsible_content" style="display: none;">
 		            <p> A player cannot throw the same number and it must be an even number, that is greater or lower by 1. For example, if 5 is thrown 4 or 6 can be thrown</p>
@@ -70,7 +70,7 @@
 		
 		<div class="grid-container">
 			<div class="cards">
-				<h1 class="game_Title" align="center">Even Number thrown</h1>
+				<h1 align="center">Even Number thrown</h1>
 				<button class="collapsible">More information</button>
 	            <div class="collapsible_content" style="display: none;">
 		            <p> A player cannot throw the same number and it must be an odd number, that is greater or lower by 1. For example, if 6 is thrown user can use 5 or 7</p>
@@ -80,6 +80,88 @@
 			    <img src="Images/6_Heart.png" alt="6 Of Hearts" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
 		    </div>
 		</div>
+		
+		<div class="grid-container">
+			<div class="cards">
+				<h1 align="center">Same Number & Same Colour</h1>
+				<button class="collapsible">More information</button>
+	            <div class="collapsible_content" style="display: none;">
+		            <p> If you have cards with same numbers and same colour, you can throw all of them at once. But not if the colour or numbers are differents</p>
+		        </div>
+		    </div>
+			<div class="img">
+			    <img src="Images/3_Yellow.jpg" alt="Yellow 3" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
+		    </div>
+		</div>
+		
+		<div class="grid-container">
+			<div class="cards">
+				<h1 align="center">Block cards</h1>
+				<button class="collapsible">More information</button>
+	            <div class="collapsible_content" style="display: none;">
+		            <p> If a block card is thrown to you, you miss your turn, and its the next players turn</p>
+		        </div>
+		    </div>
+			<div class="img">
+			    <img src="Images/Block_card.png" alt="Block card" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
+		    </div>
+		</div>
+		
+		<div class="grid-container">
+			<div class="cards">
+				<h1 align="center">Change Number card</h1>
+				<button class="collapsible">More information</button>
+	            <div class="collapsible_content" style="display: none;">
+		            <p> If you throw a change number card, you can choose the number that you want, and if the other player doesn’t have it, they will have to pick one card and miss their turn</p>
+		        </div>
+		    </div>
+			<div class="img">
+			    <img src="Images/change_number.jpg" alt="Change number card" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
+		    </div>
+		</div>
+		
+		<div class="grid-container">
+			<div class="cards">
+				<h1 align="center">Reverse Card</h1>
+				<button class="collapsible">More information</button>
+	            <div class="collapsible_content" style="display: none;">
+		            <p> If a reverse card is thrown to you, you miss your turn, and the play goes to the other direction. For example, if it was going to the left, the direction will change to the right</p>
+		        </div>
+		    </div>
+			<div class="img">
+			    <img src="Images/reverse_card.jpg" alt="Reverse card" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
+		    </div>
+		</div>
+		
+		<div class="grid-container">
+			<div class="cards">
+				<h1 align="center">+Three Card</h1>
+				<button class="collapsible">More information</button>
+	            <div class="collapsible_content" style="display: none;">
+		            <p> When you throw a +Three card you will have the power to choose the colour and number that you want, if the other player has the number and the colour you chose, then you will have to pick 2 cards and if they dont have it they will have to pick 3 cards, if the player doesn’t have another +3 card to stack</p>
+		        </div>
+		    </div>
+			<div class="img">
+			    <img src="Images/plus3.png" alt="Plus 3 card" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
+		    </div>
+		</div>
+		
+		<div class="grid-container">
+			<div class="cards">
+				<h1 align="center">Question Card</h1>
+				<button class="collapsible">More information</button>
+	            <div class="collapsible_content" style="display: none;">
+		            <p> If a question mark card is thrown to you, you will have to answer a question chosen by the player, otherwise you will have to pick 4 cards.</p>
+		        </div>
+		    </div>
+			<div class="img">
+			    <img src="Images/question_carc.jpg" alt="Question card" style="width:300px;height:250px; display: block; margin-left: auto; margin-right: auto; padding-top: 30px">
+		    </div>
+		</div>
+		
+		
+		
+		
 		
 		
 		
