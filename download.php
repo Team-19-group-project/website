71eddf28fbf34cc4fd0509297f445a8ce3d0b934
JeevanS1 <93318486+JeevanS1@users.noreply.github.com>
2021-12-15 15:27:07 +0000

    Add files via upload

---
+++ download.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+	    <title>Download</title>
+		<!-- Loads the css code needed to add style to each page, changing this file would have an impact on all pages-->
+	    <link href="css/style.css" rel="stylesheet">
+	</head>
+    <body>
+		<h1 class="game_Title" align="center">+Three</h1>
+		<!-- Navigation bar provides the ability to change pages-->
+	    <div class="navbar">		
+        <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Register.php">Register</a></li>
+		<li><a href="Login.php">Login</a></li>
+		<li><a class="active" href="download.php">Download</a></li>
+        </ul>
+		</div>
+		<h1 class="aston_Title" align="center"> Download + Three</h1>
+		
+		<a href="Game/CurrentBuild_14.12.21.zip" download>
+			<img src="Images/game_table.png" alt="Layout of game" width="250" height="250">
+		</a>
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+    </body>
+</html>
+
--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <li><a class="active" href="index.php">Home</a></li>
         <li><a href="Register.php">Register</a></li>
 		<li><a href="Login.php">Login</a></li>
+		<li><a href="download.php">Download</a></li>
         </ul>
 		</div>
 		<h1 class="aston_Title" align="center"> The Newest Card Game Launching Soon....</h1>
