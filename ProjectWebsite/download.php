5402e70e2c321244ad62b9c5514d9f679ea6c2bf
JeevanS1 <93318486+JeevanS1@users.noreply.github.com>
2022-03-03 18:20:08 +0000

    Add files via upload

---
+++ ProjectWebsite/download.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="utf-8">
+	    <title>Download</title>
+		<!-- Loads the css code needed to add style to each page, changing this file would have an impact on all pages-->
+	    <link href="css/style.css" rel="stylesheet">
+	</head>
+    <body>
+		<h1 class="game_Title" align="center">+Three</h1>
+		<!-- Navigation bar provides the ability to change pages-->
+	    <div class="navbar">		
+        <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Register.php">Register</a></li>
+		<li><a href="Login.php">Login</a></li>
+		<li><a href="about.php">About Us</a></li>
+		<li><a class="active" href="download.php">Download</a></li>
+        </ul>
+		</div>
+		<h1 class="aston_Title" align="center"> Download + Three - Click on the image below to start download</h1>
+		<br>
+		<br>
+		<br>
+		<a href="https://plusthreedownload.s3.eu-west-2.amazonaws.com/%2B3.zip" download>
+			<center><img src="Images/download_button.png" alt="Download Button" width="500" height="150"></center>
+		</a>
+	
+    </body>
+</html>
+
