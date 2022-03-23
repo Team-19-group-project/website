b133c25518c6802da4be208de630d8a2f3f09d10
JeevanS1 <93318486+JeevanS1@users.noreply.github.com>
2022-03-23 14:58:05 +0000

    Add files via upload

--- about.php
+++ about.php
@@ -118,7 +118,7 @@
 				<div class="flip-card">
 					<div class="flip-card-inner">
 						<div class="flip-card-front">
-							<img src="Images/img_Talha.PNG" alt="Image of Talha Khalid" style="width:250px;height:250px;">
+							<img src="Images/img_Talha.png" alt="Image of Talha Khalid" style="width:250px;height:250px;">
 						</div>
 						<div class="flip-card-back">
 							<h1>Talha Khalid</h1> 
