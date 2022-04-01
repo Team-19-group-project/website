5c987c1dde3804928c0ca3fb1d823e66efd88056
JeevanS1 <93318486+JeevanS1@users.noreply.github.com>
2022-04-01 10:14:37 +0100

    Add files via upload

--- about.php
+++ about.php
@@ -22,6 +22,7 @@
 		
 		<h1 class="aston_Title" align="center"> Hover over the images to learn more about us</h1>
 		
+		<p><center> We are a group of students from Aston University that have created an exciting card game for our team project that has been in development since September</center><p>
 		
 		<!--Card template used from w3schools.com-->
 		<div class = "row"> 
